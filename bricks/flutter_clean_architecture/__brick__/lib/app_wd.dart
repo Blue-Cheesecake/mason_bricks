adea2fdc2b8dd8b23a03a9894569a9a158c2d0aa
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-01-07 01:29:43 +0700

    feat: FCA-0 main run app

---
+++ bricks/flutter_clean_architecture/__brick__/lib/app_wd.dart
@@ -0,0 +1,38 @@
+{{^is_using_riverpod}}
+import 'package:flutter/material.dart';
+
+{{/is_using_riverpod}}
+
+{{#is_using_riverpod}}
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+{{/is_using_riverpod}}
+
+import 'lib/core/core.dart';
+
+class AppWD extends StatelessWidget {
+  const AppWD({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    {{^is_using_riverpod}}
+    return MaterialApp.router(
+        routerConfig: {{project_key}}Navigation.instance.router,
+        debugShowCheckedModeBanner: false,
+    );
+
+    {{/is_using_riverpod}}
+
+    {{#is_using_riverpod}}
+    return ProviderScope(
+      child: MaterialApp.router(
+        routerConfig: {{project_key}}Navigation.instance.router,
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+
+    {{/is_using_riverpod}}
+  }
+}
