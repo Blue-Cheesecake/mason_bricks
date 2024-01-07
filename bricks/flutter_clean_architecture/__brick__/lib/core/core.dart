7b5a54678495b84cc67d63a6a66e1e9630c813ba
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-01-07 15:14:56 +0700

    Resolve FCA-0 "Feature/ secure storage"
    * feat: FCA-0 impl secure storage
    
    * feat: FCA-0 export files
    
    * feat: FCA-0 export userss
    
    * feat: FCA-0 impl app user token

--- bricks/flutter_clean_architecture/__brick__/lib/core/core.dart
+++ bricks/flutter_clean_architecture/__brick__/lib/core/core.dart
@@ -1,4 +1,6 @@
 export 'app_config/app_config.dart';
 export '{{project_key.lowerCase()}}_logger/{{project_key.lowerCase()}}_logger.dart';
 export '{{project_key.lowerCase()}}_navigation/{{project_key.lowerCase()}}_navigation.dart';
+export '{{project_key.lowerCase()}}_secure_storage/{{project_key.lowerCase()}}_secure_storage.dart';
 export '{{project_key.lowerCase()}}_theme/{{project_key.lowerCase()}}_theme.dart';
+export '{{project_key.lowerCase()}}_user/{{project_key.lowerCase()}}_user.dart';
