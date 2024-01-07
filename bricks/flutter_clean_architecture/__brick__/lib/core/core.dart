c1bae7b937c77f92acd2c7cb0c871cda0d28f447
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-01-07 15:08:49 +0700

    feat: FCA-0 export files

--- bricks/flutter_clean_architecture/__brick__/lib/core/core.dart
+++ bricks/flutter_clean_architecture/__brick__/lib/core/core.dart
@@ -1,4 +1,5 @@
 export 'app_config/app_config.dart';
 export '{{project_key.lowerCase()}}_logger/{{project_key.lowerCase()}}_logger.dart';
 export '{{project_key.lowerCase()}}_navigation/{{project_key.lowerCase()}}_navigation.dart';
+export '{{project_key.lowerCase()}}_secure_storage/{{project_key.lowerCase()}}_secure_storage.dart';
 export '{{project_key.lowerCase()}}_theme/{{project_key.lowerCase()}}_theme.dart';
